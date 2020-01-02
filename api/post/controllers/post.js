fd9c8c44e10aa864eae944e4b6567588bcb2ba2b
Sergio Valencia <serg337@gmail.com>
2020-01-02 23:02:30 +0100

    implemented education, experience, comment and like endpoints and controller logic

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,44 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  //Add a comment to post
+  addComment: async ctx => {
+    const { user } = ctx.state;
+    let entity = await strapi.services.post.findOne(ctx.params);
+    let post = sanitizeEntity(entity, { model: strapi.models.post });
+    delete post.likes;
+
+    post.comments = post.comments.map(comment => {
+      delete comment._id;
+      return comment;
+    });
+
+    post.comments.push({ ...ctx.request.body, user: user.id });
+    entity = await strapi.services.post.update(ctx.params, post);
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+
+  addLike: async ctx => {
+    const { user } = ctx.state;
+    let entity = await strapi.services.post.findOne(ctx.params);
+    let post = sanitizeEntity(entity, { model: strapi.models.post });
+    delete post.comments;
+    console.log(post.likes);
+
+    post.likes = post.likes.map(like => {
+      delete like._id;
+
+      return like;
+    });
+
+    post.likes.push({ ...ctx.request.body, user: user.id });
+    entity = await strapi.services.post.update(ctx.params, post);
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  }
+};
--- api/profile/controllers/profile.js
+++ api/profile/controllers/profile.js
@@ -1,6 +1,7 @@
 const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
+  // Create Profile linked to user ID
   async create(ctx) {
     const { user } = ctx.state;
     const entity = await strapi.services.profile.create({
@@ -11,16 +12,7 @@ module.exports = {
     return sanitizeEntity(entity, { model: strapi.models.profile });
   },
 
-  // async update(ctx) {
-  //   const { user } = ctx.state;
-  //   const entity = await strapi.services.profile.update(
-  //     { ...ctx.params, user: user.id },
-  //     ctx.request.body
-  //   );
-
-  //   return sanitizeEntity(entity, { model: strapi.models.profile });
-  // }
-
+  // Delete Profile linked to user ID
   async delete(ctx) {
     const { user } = ctx.state;
     const entity = await strapi.services.profile.delete({
@@ -28,5 +20,56 @@ module.exports = {
       user: user.id
     });
     return sanitizeEntity(entity, { model: strapi.models.profile });
+  },
+
+  //add entry object to Education Array
+  addEducation: async ctx => {
+    let entity = await strapi.services.profile.findOne(ctx.params);
+
+    if (entity.user.id === ctx.state.user.id) {
+      let profile = sanitizeEntity(entity, { model: strapi.models.profile });
+      delete profile.user;
+      delete profile.experience;
+
+      profile.education = profile.education.map(education => {
+        delete education._id;
+        return education;
+      });
+
+      profile.education.push(ctx.request.body);
+      entity = await strapi.services.profile.update(ctx.params, profile);
+
+      return sanitizeEntity(entity, { model: strapi.models.profile });
+    } else {
+      return { error: "You are not allowed to do that" };
+    }
+  },
+
+  //add entry object to Experience Array
+  addExperience: async ctx => {
+    let entity = await strapi.services.profile.findOne(ctx.params);
+
+    if (entity.user.id === ctx.state.user.id) {
+      let profile = sanitizeEntity(entity, { model: strapi.models.profile });
+      delete profile.user;
+      delete profile.education;
+
+      profile.experience = profile.experience.map(experience => {
+        delete experience._id;
+        return experience;
+      });
+      profile.experience.push(ctx.request.body);
+      entity = await strapi.services.profile.update(ctx.params, profile);
+
+      return sanitizeEntity(entity, { model: strapi.models.profile });
+    } else {
+      return { error: "You are not allowed to do that" };
+    }
+  },
+
+  //Delete education item from profile
+  removeEducation: async ctx => {
+    let entity = await strapi.services.profile.findOne(ctx.params);
+    console.log(entity);
   }
 };
